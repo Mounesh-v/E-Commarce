1f468c862ed1f65a976901baa4d8277c1f64705d
Mounesh V <mouneshv696@gmail.com>
2026-05-12 16:36:32 +0530

    Added the Deployed Url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://e-commarce-5k6i.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
